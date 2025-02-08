fc5859dd8aa4c31e04bda529d1a2434192a892de
Lucky <lucky123@gmail.com>
2025-02-08 18:37:03 +0530

    hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,18 +20,15 @@ export default function Home() {
 <div className="flex flex-col items-center"> 
 
 
-        
-          <h1 className="md:text-6xl text-2xl text-center md:mt-12 font-bold">
-          Just grab, insert, and let your<br /> {" "}
-           
-          </h1>
-          <p className="text-md md:text-lg text-center mt-6 mb-5 text-gray-400">
-            Accelerate your project’s growth with ready-to-use UI components{" "}
-            <br /> that save time and elevate quality
-          </p>
-          <h1 className="text-center font-bold mt-6 text-2xl text-gray-400">
-            Built With
-          </h1>
+<Button size={'lg'} className=" p-8 mb-8 md:mb-0 text-2xl w-full sm:w-fit border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group  transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500">
+                <span className=" bg-clip-text text-transparent bg-gradient-to-r from-neutral-500 md:text-center font-sans group-hover:bg-gradient-to-r group-hover:from-black group-hover:to-black">
+                  Start For Free Today
+                </span>
+
+              </Button>
+              <h1 className=" text-5xl md:text-3xl bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-bold">
+                Boost Productivity Effortlessly with BotNest
+              </h1>
      
 
 </div>
