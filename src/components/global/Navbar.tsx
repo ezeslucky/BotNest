f3e7539d352fc1164ba47facafb8819d08a84a01
Lucky <lucky123@gmail.com>
2025-02-09 19:09:51 +0530

    footer

--- src/components/global/Navbar.tsx
+++ src/components/global/Navbar.tsx
@@ -3,6 +3,9 @@ import Link from 'next/link'
 import React from 'react'
 import { MenuIcon } from 'lucide-react'
 import { UserButton, currentUser } from '@clerk/nextjs'
+import { Icons } from './icon'
+import { ModeToggle } from './mode-toggle'
+import { buttonVariants } from '../ui/button'
 
 type Props = {}
 
@@ -11,15 +14,15 @@ const Navbar = async (props: Props) => {
   return (
     <header className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between">
       <aside className="flex items-center gap-[2px]">
-        <p className="text-3xl font-bold">Fu</p>
+        {/* <p className="text-3xl font-bold">Fu</p> */}
         <Image
-          src="/BotNestLogo.png"
-          width={15}
-          height={15}
-          alt="BotNest logo"
+          src="/logo.svg"
+          width={30}
+          height={30}
+          alt="BotNest "
           className="shadow-sm"
         />
-        <p className="text-3xl font-bold">zie</p>
+        <p className="text-3xl font-bold">BotNest</p>
       </aside>
       <nav className="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] hidden md:block">
         <ul className="flex items-center gap-4 list-none">
@@ -43,7 +46,26 @@ const Navbar = async (props: Props) => {
           </li>
         </ul>
       </nav>
+      
       <aside className="flex items-center gap-4">
+      <div className=' flex items-center gap-2'>
+              <Link
+                href="https://github.com/ezeslucky/BotNest.git"
+                className={buttonVariants({ variant: "ghost", size: "icon" })}
+              >
+                <Icons.gitHub className="icon-class w-4" />
+              </Link>
+              <Link
+                href="https://x.com/ezeslucky"
+                className={buttonVariants({
+                  variant: "ghost",
+                  size: "icon",
+                })}
+              >
+                <Icons.twitter className="icon-class w-3" />
+              </Link>
+              <ModeToggle />
+              </div>
         <Link
           href="/dashboard"
           className="relative inline-flex h-10 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
