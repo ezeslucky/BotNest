f04be758ed43a3b7b5d1418b554c3e4c149a122a
Lucky <lucky123@gmail.com>
2025-02-12 18:56:52 +0530

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import { Button } from '@/components/ui/button'
 import { clients, products } from '@/lib/constant'
 import { CheckIcon } from 'lucide-react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   //WIP: remove fault IMAge for home page
@@ -26,7 +27,8 @@ export default function Home() {
                   className="p-8 mb-8 md:mb-0 text-2xl w-full sm:w-fit border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500"
                 >
                   <span className="bg-clip-text text-transparent bg-gradient-to-r from-neutral-500 to-neutral-600  md:text-center font-sans group-hover:bg-gradient-to-r group-hover:from-black goup-hover:to-black">
-                    Start For Free Today
+                    <Link href='/dashboard'>Start For Free Today</Link>
+                    
                   </span>
                 </Button>
                 <h1 className="text-5xl md:text-8xl  bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-bold">
